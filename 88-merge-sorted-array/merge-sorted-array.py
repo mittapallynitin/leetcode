e547d1f331db67387c14249824b92687233a644d
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-04 00:20:28 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (99.30%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -6,19 +6,15 @@ class Solution:
         i, j = m-1, n - 1
         p = m + n - 1
 
-        while i > - 1 and j > -1:
-            if nums1[i] >= nums2[j]:
+        while j >= 0:
+            if i >= 0 and nums1[i] >= nums2[j]:
                 nums1[p] = nums1[i]
                 i -= 1
             else:
                 nums1[p] = nums2[j]
                 j -= 1
             p -= 1
-        while i > -1:
-            nums1[p] = nums1[i]
-            p -= 1
-            i -= 1
-        while j >-1:
+        while j >= 0:
             nums1[p] = nums2[j]
             p -= 1
             j -= 1
