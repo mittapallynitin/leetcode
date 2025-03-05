a606e31272f97ac7b361db724639b14b7fdb5cb6
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-05 00:27:12 -0500

    Time: 540 ms (10.28%) | Memory: 18.1 MB (77.77%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -6,10 +6,10 @@ class Solution:
                 if key not in tracker or tracker[key] < vals[key]:
                     return False
             return True
-        vals = {}
-        required = set(t)
+    
+        required = {}
         for ch in t:
-            vals[ch] = vals.get(ch, 0) + 1
+            required[ch] = required.get(ch, 0) + 1
         l = 0 
         min_window = float("inf")
         tracker = {}
@@ -17,13 +17,11 @@ class Solution:
         for r in range(len(s)):
             if s[r] in required:
                 tracker[s[r]] = tracker.get(s[r], 0) + 1
-            while isValid(tracker, vals):
+            while isValid(tracker, required):
                 if r - l + 1 < min_window:
                     min_window = r - l +1
                     answer = s[l:r+1]
                 if s[l] in required:
                     tracker[s[l]] -= 1
-                    if tracker[s[l]] == 0:
-                        del tracker[s[l]]
                 l += 1
         return answer
