496f2ea2dfc10dae4d1a07bfce05bd83b3352699
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-08 18:06:17 -0500

    Time: 14 ms (73.35%) | Memory: 19.8 MB (91.78%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -3,7 +3,7 @@ class Solution:
         kids = len(ratings)
         total_candies = [1]*kids
         for i in range(1, kids):
-            if (ratings[i-1] < ratings[i]) and (total_candies[i] <= total_candies[i-1]):
+            if (ratings[i-1] < ratings[i]):
                 total_candies[i] = total_candies[i-1] + 1
         for i in range(kids-2, -1, -1):
             if (ratings[i+1] < ratings[i]) and (total_candies[i] <= total_candies[i+1]):
