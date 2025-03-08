595ebabad54a38f4ff184d61b664a3227e34085b
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-08 18:45:59 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (8.41%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,10 +1,11 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         ans = ""
-        for chrs in zip(*map(list,strs)):
-            ch = chrs[0]
-            if all(map(lambda x: x == ch, chrs)):
-                ans += ch
+        strs = sorted(strs)
+        first, last = strs[0], strs[-1]
+        for ch1, ch2 in zip(first, last):
+            if ch1 == ch2:
+                ans += ch1
             else:
                 break
         return ans
