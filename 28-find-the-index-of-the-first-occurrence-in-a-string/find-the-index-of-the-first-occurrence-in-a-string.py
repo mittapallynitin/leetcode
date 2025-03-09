aa86d7603c41c90008636abdaee15dde59ca14c4
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-09 03:25:09 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (51.67%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,14 +3,13 @@ class Solution:
         haystack_size = len(haystack)
         needle_size = len(needle)
 
+        if haystack_size < needle_size:
+            return -1
+
         for ans in range(haystack_size):
-            hay_p = ans
-            ned_p = 0
-            while hay_p < haystack_size and haystack[hay_p] == needle[ned_p]:
-                hay_p += 1
-                ned_p += 1
-                if ned_p == len(needle):
-                    return ans
+            if haystack[ans: ans+needle_size] == needle:
+                return ans
+
         return -1
 
 
