0647e89d2de6ca83f2af2b7f597079be4bb8970c
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-10 17:55:50 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.87%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -1,20 +1,26 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        output = []
-        prev = float("-inf")
-        i = 0
-        while i < len(nums):
-            start = nums[i]
-            end = nums[i]
-            curr = nums[i]
-            while i+1 < len(nums) and curr + 1 == nums[i+1]:
-                i += 1
-                end = curr = nums[i]
-            if end != start:
-                output.append(f"{start}->{end}")
+        # output = []
+        # i = 0
+        # while i < len(nums):
+        #     start = nums[i]
+        #     end = nums[i]
+        #     curr = nums[i]
+        #     while i+1 < len(nums) and curr + 1 == nums[i+1]:
+        #         i += 1
+        #         end = curr = nums[i]
+        #     if end != start:
+        #         output.append(f"{start}->{end}")
+        #     else:
+        #         output.append(str(start))
+        #     i += 1
+        # return output
+        ranges = []
+        for n in nums:
+            if ranges and ranges[-1][1] == n -1:
+                ranges[-1][1] = n
             else:
-                output.append(str(start))
-            i += 1
-        return output
+                ranges.append([n, n])
+        return [f"{s}->{e}" if s != e else str(s) for s, e in ranges]
 
         
