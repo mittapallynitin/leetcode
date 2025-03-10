e1ded70f0870a73a4d8b452ac08de76a32eff4ff
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-10 18:11:58 -0400

    Time: 7 ms (73.24%) | Memory: 21.6 MB (33.87%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,10 +3,11 @@ class Solution:
         ranges = []
         intervals = sorted(intervals, key=lambda x: x[0])
         for s, e in intervals:
-            if not ranges or ranges[-1][1] < s:
-                ranges.append([s, e])
-            else:
+            if ranges and ranges[-1][1] >= s:
                 ranges[-1][1] = max(ranges[-1][1], e)
+            else:
+                ranges.append([s, e])
+                
                 
         return ranges
         
