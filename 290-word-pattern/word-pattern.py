2245d391bf3391cfb98c086a4b10fedb5a5524d4
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-03-10 15:50:20 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (16.50%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,20 +1,10 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        pw_map = dict()
-        wp_map = dict()
-
         words = s.split()
-
         if len(pattern) != len(words): return False
 
-        for pt, word in zip(pattern, words):
-
-            if (
-                (pt in pw_map and pw_map[pt] != word) or 
-                (word in wp_map and wp_map[word] != pt)
-            ):
-                return False
-
-            pw_map[pt] = word
-            wp_map[word] = pt
-        return True
+        return (
+            len(set(pattern)) == 
+            len(set(words)) ==
+            len(set(zip(pattern, words)))
+        )
