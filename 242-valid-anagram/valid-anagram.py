1d2193f229f4d35b1d7b9e25fa7384f034594e46
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-05-22 19:42:43 -0400

    Time: 11 ms (72.54%) | Memory: 18 MB (32.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,14 +3,12 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        alpha = [0]*26
+        alpha = dict()
         for ch1, ch2 in zip(s, t):
-            ch1 = ord(ch1) - ord('a')
-            ch2 = ord(ch2) - ord('a')
-            alpha[ch1] += 1
-            alpha[ch2] -= 1
+            alpha[ch1] = alpha.get(ch1, 0) +  1
+            alpha[ch2] = alpha.get(ch2, 0) - 1
 
-        for i in range(26):
+        for i in alpha:
             if alpha[i] != 0:
                 return False
         return True
