b526af98c5497390a898e243f680471d6fece6de
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-05-22 18:40:43 -0400

    Time: 44 ms (17.01%) | Memory: 18 MB (8.22%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,7 +1,7 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = ""
         n = min(len(word1), len(word2))
+        res = ""
         for i in range(n):
             res += word1[i] + word2[i]
         res += word1[n:] + word2[n:]
