92172b8523d89c5771ca044ff7a9c040e8970c69
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-05-22 23:04:54 -0400

    Time: 1 ms (33.05%) | Memory: 18 MB (59.87%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        score = []
+        for op in operations:
+            if op == "C" and score:
+                score.pop()
+            elif op == "+" and score:
+                score.append(
+                    score[-2] + score[-1]
+                )
+            elif op == "D" and score:
+                score.append(
+                    score[-1]*2
+                )
+            else:
+                score.append(int(op))
+        
+        res = 0 
+        for s in score:
+            res += s
+        return res
