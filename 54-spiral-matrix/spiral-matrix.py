5cd412262e70ce36b26b81d2ddad30e943237fd1
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-05-26 18:49:50 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (62.51%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,31 +1,30 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        output = []
-        ROWS, COLS = len(matrix), len(matrix[0])
-        VISITED = 101
+        top, bottom = 0, len(matrix)
+        left, right = 0, len(matrix[0])
+        res = []
 
-        # Inital Movement: Increment column wise
-        dr = 0; dc = 1
-        switch_map = {
-            (1,0): (0, 1), # Switch moving right to moving bottom. 
-            (0,1): (-1, 0), # Bottom -> left
-            (-1,0): (0, -1), # Left -> Top 
-            (0, -1): (1, 0) # Top -> Right
-        }
-        r, c = 0, 0
+        while top < bottom and left < right:
+            # Move right:
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
 
+            # Move Down
+            for i in range(top, bottom):
+                res.append(matrix[i][right-1])
+            right -= 1
 
-        for _ in range(ROWS*COLS):        
-            output.append(matrix[r][c])
-            matrix[r][c] = VISITED
-            nr = r + dr
-            nc = c + dc
-            # switch direction
-            if (nr == ROWS) or (nc == COLS) or matrix[nr][nc] == VISITED:
-                dc, dr = switch_map[(dc, dr)]
-                nr = r + dr
-                nc = c + dc
-            
-            r, c = nr, nc
+            if not (top < bottom and left < right): break
+
+            # Move left
+            for i in range(right-1, left-1, -1):
+                res.append(matrix[bottom-1][i])
+            bottom -= 1
+
+            # Move up
+            for i in range(bottom-1, top-1, -1):
+                res.append(matrix[i][left])
+            left += 1
         
-        return output
+        return res
