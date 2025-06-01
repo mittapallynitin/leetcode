e5570c13bde2e7afa5b83252a3d548bdcb38a5cb
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-05-31 23:37:03 -0400

    Time: 615 ms (8.03%) | Memory: 18.5 MB (16.24%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,27 +1,50 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        from collections import defaultdict
 
-        def isValid(tracker, vals):
-            for key in vals:
-                if key not in tracker or tracker[key] < vals[key]:
+        # S Details
+        seq_len = len(s)
+        
+        # T Details
+        char_hash = defaultdict(int)
+        required_len = len(t)
+        for ch in t:
+            char_hash[ch] += 1
+
+        # Build details
+        build_hash = defaultdict(int)
+        build_len = 0
+
+        # Result
+        window = float("inf")
+        answer = ""
+        l = 0 
+
+        # Validate window
+        def is_valid(build_hash):
+            for ch in char_hash:
+                if build_hash.get(ch, 0) < char_hash[ch]:
                     return False
             return True
-    
-        required = {}
-        for ch in t:
-            required[ch] = required.get(ch, 0) + 1
-        l = 0 
-        min_window = float("inf")
-        tracker = {}
-        answer = ""
-        for r in range(len(s)):
-            if s[r] in required:
-                tracker[s[r]] = tracker.get(s[r], 0) + 1
-            while isValid(tracker, required):
-                if r - l + 1 < min_window:
-                    min_window = r - l +1
-                    answer = s[l:r+1]
-                if s[l] in required:
-                    tracker[s[l]] -= 1
+
+
+        for r in range(seq_len):
+            if s[r] in char_hash:
+                build_hash[s[r]] += 1
+                build_len += 1
+
+            while build_len >= required_len and is_valid(build_hash):
+                window_len = r - l + 1
+                if window_len < window:
+                    window = window_len
+                    answer = s[l:r + 1]
+
+                left_ch = s[l]
+                if left_ch in build_hash:
+                    build_hash[left_ch] -= 1
+                    build_len -= 1
                 l += 1
+        
         return answer
+
+
