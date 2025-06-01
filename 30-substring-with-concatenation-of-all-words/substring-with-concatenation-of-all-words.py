51fba598a7cdad44ace05bb3a27832551c6041f5
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-05-31 22:25:24 -0400

    Time: 15 ms (97.47%) | Memory: 18.9 MB (20.70%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,21 +1,26 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        from collections import Counter, defaultdict
-        word_len = len(words[0])
+        from collections import defaultdict
+
         seq_len = len(s)
+
+        word_len = len(words[0])
         n_words = len(words)
-        total_len = word_len * n_words
         word_hash = defaultdict(int)
+        for word in words:
+            word_hash[word] += 1
+        
         build_hash = defaultdict(int)
-        for word in words: word_hash[word] += 1
         result = []
 
-        for i in range(word_len):
-            left = i
+        for offset in range(word_len):
+            left = offset
             build_hash.clear()
             count = 0
-            for j in range(i, seq_len - word_len+1, word_len):
-                chunk = s[j: j+word_len]
+
+            for right in range(left, seq_len - word_len + 1, word_len):
+
+                chunk = s[right: right + word_len]
                 if chunk in word_hash:
                     build_hash[chunk] += 1
                     count += 1
@@ -28,11 +33,8 @@ class Solution:
                     
                     if count == n_words:
                         result.append(left)
-                
-                else:
+                else: 
                     build_hash.clear()
-                    count = 0
-                    left = j + word_len
+                    count = 0 
+                    left = right + word_len
         return result
-                
-
