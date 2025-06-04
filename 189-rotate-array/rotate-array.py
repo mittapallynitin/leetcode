2fa768ba1b309132b54cec960175421444d9758f
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-06-04 18:42:16 -0400

    Time: 11 ms (22.71%) | Memory: 25.6 MB (75.76%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,10 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Way 1 : Uses O(N) memory for O(N) time complexity
+        # k = k % len(nums)
+        # first = nums[:-k]
+        # second = nums[-k:]
+        # nums[::] = second + first
+
+        # Way 2: Reverse the complete list, revese the first k and rest of elements. 
+        def reverse_inplace(nums, start, stop):
+            n_elemts = (stop - start)
+            n = n_elemts // 2 + n_elemts % 2
+
+            for i in range(n):
+                nums[start + i], nums[stop-i-1] = nums[stop-i-1], nums[start + i]
+        
+        if len(nums) == 1:
+            return
         k = k % len(nums)
-        first = nums[:-k]
-        second = nums[-k:]
-        nums[::] = second + first
+        reverse_inplace(nums, 0, len(nums))
+        reverse_inplace(nums, 0, k)
+        reverse_inplace(nums, k, len(nums))
+        print(nums)
+            
+
+
+
 
 
         
