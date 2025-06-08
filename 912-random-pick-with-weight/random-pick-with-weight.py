dccdb462ff83e23a3ced40612cc0a20b84665f25
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-06-08 17:40:03 -0400

    Time: 2521 ms (13.11%) | Memory: 23.5 MB (16.86%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        total_weight = float(sum(w))
+        self.probs = [weight/total_weight for weight in w]
+        self.index = []
+        curr = 0
+        for prob in self.probs:
+            curr += prob
+            self.index.append(curr)
+
+
+    def pickIndex(self) -> int:
+        from random import random
+        x = random()
+        for i, prob in enumerate(self.index):
+            if x < prob:
+                return i
+
+        
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
