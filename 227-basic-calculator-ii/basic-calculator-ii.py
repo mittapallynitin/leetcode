0bd833aa5b5f676a02c71ddc9bf6d9bb9583caa4
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-06-08 16:54:00 -0400

    Time: 137 ms (9.06%) | Memory: 23.1 MB (9.86%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,43 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s.replace(" ", "")
+        stack = []
+        i = 0
+        def get_op(i):
+            result = ""
+            while i < len(s) and s[i].isdigit():
+                result += s[i]
+                i += 1
+            
+            return int(result), i
+
+        while i < len(s):
+            if s[i] == " ":
+                i += 1
+                continue
+            elif s[i] in "+-":
+                stack.append(s[i])
+                i += 1
+            elif s[i] in "*/":
+                op = s[i]
+                op1 = stack.pop()
+                op2, i = get_op(i+1) 
+                if op == "/":
+                    stack.append(op1//op2)
+                else:
+                    stack.append(op1*op2)
+            else:
+                op, i = get_op(i)
+                stack.append(op)
+        
+        curr = 0 
+        sign = 1
+        for op in stack:
+            if op == "-":
+                sign = -1
+            elif isinstance(op, int):
+                curr += sign * op
+                sign = 1
+        return curr
+                
+        
