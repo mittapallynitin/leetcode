33bc05c01ecdbe873565b51fa97c5c0d9e3f971c
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-06-16 15:28:02 -0400

    Time: 2 ms (52.31%) | Memory: 18 MB (23.54%) - LeetSync

--- 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.py
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.py
@@ -3,7 +3,11 @@ class Solution:
         total = len(s)
         chrs = sorted([(ch, cnt) for ch, cnt in Counter(s).items()], key=lambda x: x[1])
         deletions = 0
-        while len(chrs) > k:
-            ch, cnt = chrs.pop(0)
+        index = 0
+        uniq = len(chrs)
+        while uniq > k and index < len(chrs):
+            ch, cnt = chrs[index]
             deletions += cnt
+            index += 1
+            uniq -= 1
         return deletions
