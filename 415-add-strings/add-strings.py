6408586aa5d9c4ee077afce2ed3f60cdbfb448c4
Nitin Mittapally <mittapally.nitin@gmail.com>
2025-06-19 01:11:50 -0400

    Time: 7 ms (46.08%) | Memory: 18.3 MB (24.58%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,11 +1,12 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         carry = 0
-        res = ""
+        res = []
         for i, j in zip_longest(num1[::-1], num2[::-1], fillvalue="0"):
-            i, j = int(i), int(j)
-            pos = (i + j + carry) % 10
-            carry = (i + j + carry) // 10
-            res += str(pos)
-        res += str(carry) if carry else ""
-        return res[::-1]
+            total = int(i) + int(j) + carry
+            pos = total % 10
+            carry = total // 10
+            res.append(str(pos))
+        if carry:
+            res.append(str(carry))
+        return "".join(reversed(res))
